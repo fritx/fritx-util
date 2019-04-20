cd3d703c0b4a571200f9bfd4bdd3dc7ae2ca3adf
Fritz Lin <uxfritz@163.com>
2019-04-20 23:33:01 +0800

    axiosUtf8/bufferUtf8 fromCharset option

--- axios-utf8.ts
+++ axios-utf8.ts
@@ -3,17 +3,34 @@ import { bufferToStream } from './helper'
 import { bufferUtf8 } from './encoding'
 import { axios } from './axios'
 
-// fixme: 应尝试通过adapter等方式实现
-export async function axiosUtf8(url: string, options?: AxiosRequestConfig): Promise<any> {
-  options = options || {}
-  const responseType = options.responseType
+interface IAxiosRequestConfigUtf8 extends AxiosRequestConfig {
+  fromCharset?: string,
+}
 
-  // todo 支持除get以为 其他方法
+// fixme: 应尝试通过adapter等方式实现
+export async function axiosUtf8(url: string, options?: IAxiosRequestConfigUtf8): Promise<any> {
+  options = options || {}
+  const { fromCharset,  ...restOptions } = options
+  const { responseType } = options
+
   const { data: rawBuffer, ...rest } = await axios.get(url, {
-    ...options,
+    ...restOptions,
     responseType: 'arraybuffer',
   })
-  const buffer = bufferUtf8(rawBuffer)
+
+  let myFromCharset = fromCharset
+
+  // todo 提出到业务逻辑 与工具方法分离
+  // 可能识别的 jschardet 不准确
+  // taobao GB2312 判定为 ISO-8859-2
+  // https://github.com/aadsm/jschardet/issues/34
+  // http://bbs.chinaunix.net/thread-616945-1-1.html
+  // https://blog.csdn.net/z1134145881/article/details/46832685
+  // https://github.com/ashtuchkin/iconv-lite/wiki/Supported-Encodings
+  const isTaobao = /[/.]item\.taobao\.com/i.test(url)
+  if (isTaobao) { myFromCharset = 'GB2312' }
+
+  const buffer = bufferUtf8(rawBuffer, myFromCharset)
   let data: any
 
   // https://github.com/axios/axios/blob/master/index.d.ts
--- encoding.ts
+++ encoding.ts
@@ -1,13 +1,17 @@
 import * as encoding from 'encoding'
 import * as jschardet from 'jschardet'
 
-export function bufferUtf8(buffer: Buffer) {
-  const ret = jschardet.detect(buffer)
-  if (!ret) {
-    throw new Error('原字符集无法识别')
-  }
+export function bufferUtf8(buffer: Buffer, fromCharset?: string) {
   const toCharset  = 'UTF-8'
-  const fromCharset = ret.encoding
+
+  if (!fromCharset) {
+    const ret = jschardet.detect(buffer)
+    if (!ret) {
+      throw new Error('原字符集无法识别')
+    }
+    fromCharset = ret.encoding
+  }
+
   if (fromCharset !== toCharset) {
     buffer = encoding.convert(buffer, toCharset, fromCharset)
   }
